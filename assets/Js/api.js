8e313d8ba2766fe72d06fae4491b3a10de1cc9a5
bishal292 <bs9107709@gmail.com>
2024-07-04 19:53:36 +0530

    Added Initial files for the project and some other details for the api

---
+++ assets/Js/api.js
@@ -0,0 +1,25 @@
+// These api are powered by OpenWeather which provide some api for free to use 
+const websiteLink ="https://openweathermap.org/";
+console.log(`This website is Powered by ${websiteLink}`);
+
+
+
+const currentWeatherApi = "https://api.openweathermap.org/data/2.5/weather?lat=51.5073219&lon=-0.1276474&appid=<API_KEY_HERE>";
+
+// This above Api will give the current weather data for the given Longitude and Latitude
+
+const forecastApi = "https://api.openweathermap.org/data/2.5/forecast?lat=51.5073219&lon=-0.1276474&appid=<API_KEY_HERE>";
+
+// This above Api will give weather forecast data for 5 days for the given Longitude and Latitude
+
+const airPollutionApi ="https://api.openweathermap.org/data/2.5/air_pollution?lat=51.5073219&lon=-0.1276474&appid=<API_KEY_HERE>"
+
+// This above Api will give the Air-Pollution data for the given Longitude and Latitude
+
+const reverseGeoApi ="https://api.openweathermap.org/geo/1.0/reverse?lat=51.5073219&lon=-0.1276474&appid=<API_KEY_HERE>"
+
+// This above Api will give the location details data like{Name, Country , State, Local_names} for the given Longitude and Latitude
+
+const geoApi ="https://api.openweathermap.org/geo/1.0/direct?q=london&appid=<API_KEY_HERE>"
+
+// This above Api will give the location details data like{Name, Country , State, Local_names} for the given place name if it is recognized globally.
